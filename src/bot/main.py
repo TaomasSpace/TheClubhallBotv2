dc134e3bb7d0c4c88958f3a515ea27d71dc751f6
TaomasSpace <155637174+TaomasSpace@users.noreply.github.com>
2025-05-13 19:17:44 +0200

    fixed problem and finished custom role

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,40 +1,23 @@
 from dotenv import load_dotenv
 import os
 import discord
-<<<<<<< Updated upstream
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.guilds = True
-intents.members = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-=======
 from discord import app_commands
 from bot_instance import bot
 import helper.events
-from Commands.boosterPearks.setBoostLevel import setboostlevel
->>>>>>> Stashed changes
+from Commands.boosterPearks import setBoostLevel, customRole
+from 
 
 load_dotenv(dotenv_path=".env")
 token = os.getenv("bot_token")
 
-<<<<<<< Updated upstream
-@bot.event
-async def on_ready():
-    await bot.tree.sync()
-    print(f"Bot is online as {bot.user}")
-
-=======
-@bot.tree.command(
-    name="setboostlevel", description="Set custom boost level manually (Admin only)"
-)
+@bot.tree.command(name="setboostlevel", description="Set custom boost level manually (Admin only)")
 @app_commands.describe(user="User", level="Boost level (1, 2, or 3)")
-async def setboostlevelCommand(
-    interaction: discord.Interaction, user: discord.Member, level: int
-):
-    await setboostlevel(interaction=interaction, user=user, level=level)
->>>>>>> Stashed changes
+async def setboostlevelCommand(interaction: discord.Interaction, user: discord.Member, level: int):
+    await setBoostLevel.setboostlevel(interaction=interaction, user=user, level=level)
+
+@bot.tree.command(name="customrole", description="Create or update your booster role")
+@app_commands.describe(name="Name of your role", color="Hex color like #FFAA00")
+async def customroleCommand(interaction: discord.Interaction, name: str, color: str):
+    await customRole.customrole(interaction=interaction, name=name, color=color)
 
 bot.run(token)
